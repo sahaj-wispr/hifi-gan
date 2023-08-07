1a842f3c5d6f59e1ae7eaab77c5757cdf00319e0
Sahaj <89000952+sahaj-wispr@users.noreply.github.com>
2023-08-06 22:11:33 -0700

    add inference functions

---
+++ inference_functions.py
@@ -0,0 +1,61 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import glob
+import os
+import numpy as np
+import argparse
+import json
+import torch
+from scipy.io.wavfile import write
+from env import AttrDict
+from meldataset import MAX_WAV_VALUE
+from models import Generator
+
+device = torch.cuda.is_available()
+
+
+def load_checkpoint(filepath, device):
+    assert os.path.isfile(filepath)
+    print("Loading '{}'".format(filepath))
+    checkpoint_dict = torch.load(filepath, map_location=device)
+    print("Complete.")
+    return checkpoint_dict
+
+
+def scan_checkpoint(cp_dir, prefix):
+    pattern = os.path.join(cp_dir, prefix + "*")
+    cp_list = glob.glob(pattern)
+    if len(cp_list) == 0:
+        return ""
+    return sorted(cp_list)[-1]
+
+
+def load_generator(checkpoint_file):
+    config_file = os.path.join(os.path.split(checkpoint_file)[0], "config.json")
+    with open(config_file) as f:
+        data = f.read()
+
+    json_config = json.loads(data)
+    config = AttrDict(json_config)
+
+    torch.manual_seed(config.seed)
+    generator = Generator(config).to(device)
+
+    state_dict_g = load_checkpoint(checkpoint_file, device)
+    generator.load_state_dict(state_dict_g["generator"])
+
+    generator.eval()
+    generator.remove_weight_norm()
+
+    return generator
+
+
+def run_inference(generator, mel):
+    with torch.no_grad():
+        x = torch.FloatTensor(mel).to(device)
+        y_g_hat = generator(x)
+        audio = y_g_hat.squeeze()
+        audio = audio * MAX_WAV_VALUE
+        audio = audio.cpu().numpy().astype("int16")
+
+    return audio
